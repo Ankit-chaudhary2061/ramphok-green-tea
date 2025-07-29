ee6e7e98156eb328dec4223d34593aa17d1f30e2
Ankit-chaudhary2061 <ankitchua2061@gmail.com>
2025-07-29 17:09:10 +0545

    feat: header & banner & about us WIP

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -11,11 +11,23 @@ const Home = () => {
 
       <div className='px-[200px] py-[100px] flex'>
         <div>
-          <h1 className='text-4xl font-bold leading-snug'>
+          <h1 className='text-[32px] font-bold leading-snug'>
             Discover <br />
             Ramfok Green <br />
             Tea
           </h1>
+          <p className='py-{32px}'>
+            Ramphok Green Tea, based in Phikkal,
+            <br /> Ilam, produces high-quality green <br />
+            and orthodox teas with strict quality control.
+            <br /> Our specialty — Pearl Green Tea — <br />
+            leads with 70% of our total output.
+          </p>
+          <button>
+            <p className='text-[16px] font-bold px-[32px] py-[16px]'>
+              Learn More
+            </p>
+          </button>
         </div>
       </div>
     </>
