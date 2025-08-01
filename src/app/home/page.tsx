461c71649e3cd971c659d98f5d86b322c8d70a8b
Ankit-chaudhary2061 <ankitchua2061@gmail.com>
2025-08-01 08:04:48 +0545

    completed the story section add story and image

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@ import Banner from "./components/Banner";
 import AboutUs from "./components/About";
 import Product from "./components/Product";
 import ProductFeature from "./components/productFeature";
+import StorySection from "./components/StorySection";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
     <AboutUs/>
     <Product/>
     <ProductFeature/>
+    <StorySection/>
     </>
   );
 };
