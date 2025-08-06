a4558570854fa1173b8c585f8662e4ee8da1c0f1
Ankit-chaudhary2061 <ankitchua2061@gmail.com>
2025-08-06 21:59:57 +0545

    refactor: renamed file, updated CTA in product banner, and moved container from pages to individual sections (SliderGallery, CeoStory, Products, ShowHideContent)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
 import AboutUs from "./home/(sections)/About";
 import Banner from "./home/(sections)/Banner";
+import StorySection from "./home/(sections)/Ceostory";
+
 import Product from "./home/(sections)/Product";
-import ProductFeature from "./home/(sections)/productFeature";
+import ProductFeature from "./home/(sections)/ProductBanner";
 import ShowHideContent from "./home/(sections)/ShowHideProduct";
 import SliderGallery from "./home/(sections)/SliderGallery";
-import StorySection from "./home/(sections)/StorySection";
+
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <div className="container mx-auto">
+      <div>
         <AboutUs />
         <Product />
         <ShowHideContent />
         <ProductFeature />
-
-        <StorySection />
+        <StorySection/>
         <SliderGallery />
       </div>
     </>
