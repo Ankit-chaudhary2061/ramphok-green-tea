3259013021d1d2d6582d005588e69b8827c4bece
Ankit-chaudhary2061 <ankitchua2061@gmail.com>
2025-08-08 22:25:27 +0545

    feat: made a curve in photo, added buttons on image, removed scroll

--- src/app/home/(sections)/SliderGallery.tsx
+++ src/app/home/(sections)/SliderGallery.tsx
@@ -16,206 +16,224 @@ import "swiper/css/scrollbar";
 
 const SliderGallery = () => {
   return (
-    <div className='container mx-auto'>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-        spaceBetween={25}
-        slidesPerView={3}
-        loop={true}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        breakpoints={{
-          320: { slidesPerView: 1 },
-          640: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-        }}
-      >
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section1.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section2.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section3.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section4.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section5.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section6.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section7.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section8.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section9.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section10.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section11.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section12.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section13.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section14.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section15.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section16.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section17.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='relative h-[419px] w-full'>
-            <Image
-              src='/images/section18.jpg'
-              alt='Green Tea'
-              className='object-cover'
-              fill
-            />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+    <div className='container mx-auto '>
+      <div className='pb-[109px]'>
+        <div className='relative h-[419px]'>
+          {/* Buttons centered vertically at 419px height */}
+          <button
+            className='swiper-button-prev-custom absolute left-3 top-1/2 -translate-y-1/2 z-10 p-3 bg-gray-300 bg-opacity-60 rounded-full shadow-md hover:bg-gray-400 hover:bg-opacity-90 transition'
+            aria-label='Previous Slide'
+          >
+            ❮
+          </button>
+          <button
+            className='swiper-button-next-custom absolute right-3 top-1/2 -translate-y-1/2 z-10 p-3 bg-gray-300 bg-opacity-60 rounded-full shadow-md hover:bg-gray-400 hover:bg-opacity-90 transition'
+            aria-label='Next Slide'
+          >
+            ❯
+          </button>
+          <Swiper
+            modules={[Navigation, A11y, Autoplay]}
+            spaceBetween={25}
+            slidesPerView={3}
+            loop={true}
+            navigation={{
+              nextEl: '.swiper-button-next-custom',
+              prevEl: '.swiper-button-prev-custom',
+            }}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            breakpoints={{
+              320: { slidesPerView: 1 },
+              640: { slidesPerView: 2 },
+              1024: { slidesPerView: 3 },
+            }}
+          >
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section1.jpg'
+                  alt='Green Tea'
+                  fill
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section2.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section3.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section4.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section5.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section6.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section7.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section8.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section9.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section10.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section11.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section12.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section13.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section14.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section15.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section16.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section17.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='relative h-[419px] w-full rounded-xl overflow-hidden'>
+                <Image
+                  src='/images/section18.jpg'
+                  alt='Green Tea'
+                  className='object-cover transition-transform duration-500 hover:scale-105'
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 };
