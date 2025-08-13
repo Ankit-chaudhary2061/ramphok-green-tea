a1e24151ebc3c5bd7e6e7563cf43b9343b39a1eb
Ankit-chaudhary2061 <ankitchua2061@gmail.com>
2025-08-13 08:18:27 +0545

    add css styles on it

--- src/app/home/(sections)/ProductBanner.tsx
+++ src/app/home/(sections)/ProductBanner.tsx
@@ -1,30 +1,34 @@
-'use client'
+'use client';
 
-import Image from "next/image";
-import AOS from "aos";
-import { useEffect } from "react";
-import "aos/dist/aos.css"; //
+import Image from 'next/image';
+import AOS from 'aos';
+import { useEffect } from 'react';
+import 'aos/dist/aos.css';
 
 const ProductFeature = () => {
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init({
       duration: 1000, // animation duration in ms
-      once: true, // animation happens only once
-      offset: 100,
+      once: false, // allow animation when scrolling up & down
+      offset: 100, // trigger distance before entering view
     });
-  },[])
+  }, []);
+
   return (
-    <>
-      <div className='w-full' data-aos='zoom-in-up'>
+    <section className='w-full  py-12 px-1'>
+      <div
+        className='max-w-[1440px] mx-auto rounded-2xl overflow-hidden shadow-lg'
+        data-aos='zoom-in-up'
+      >
         <Image
           src='/image/Flavor-from-illam.png'
           alt='GreenTea garden'
-          width={1439}
-          height={369}
-          className='w-full h-auto'
+          width={1440}
+          height={365}
+          className='w-full h-auto object-cover'
         />
       </div>
-    </>
+    </section>
   );
 };
 
