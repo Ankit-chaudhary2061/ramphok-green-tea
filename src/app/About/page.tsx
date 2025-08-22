6337e6f35bca2ff4f90bf8600f07a5a5c0bb88ab
Ankit-chaudhary2061 <ankitchua2061@gmail.com>
2025-08-23 00:40:06 +0545

    added about us

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,22 +1,8 @@
+import AboutUsSection from "../About/section/AboutUs";
+
 const About = () => {
   return (
-    <div className='min-h-screen bg-white px-4 py-16'>
-      <div className='max-w-4xl mx-auto text-center'>
-        <h1 className='text-4xl font-bold text-[#326E3B] mb-6'>About Us</h1>
-        <p className='text-lg text-gray-700 leading-7'>
-          Welcome to Ramphok Green Tea! 🌿 <br />
-          <br />
-          Our journey began with a passion for natural health and authentic
-          flavors. Sourced directly from the lush hills of Nepal, our green tea
-          is handpicked and crafted to bring you calm, clarity, and vitality.
-          <br />
-          <br />
-          Whether you&apos;re a tea lover or just starting your wellness
-          journey, we&apos;re here to share the story, purity, and power of
-          green tea — one cup at a time.
-        </p>
-      </div>
-    </div>
+    <AboutUsSection/>
   );
 };
 
